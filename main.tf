3c8b1641de07b2749af43bee5f47b88588198620
wussh <andrianta.321@gmail.com>
2024-02-11 22:25:14 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key= "${var.access_key}"
+  secret_key= "${var.secret_key}"
+}
+
+resource "tls_private_key" "rsa_4096" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name = "${var.key_name}"
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content = tls_private_key.rsa_4096.private_key_pem
+  filename = "${var.key_name}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0fa377108253bf620"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name  # Use the key pair name defined in the aws_key_pair resource block
+
+  tags = {
+    Name = "${var.ec2_name}"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+  description = "AWS Access Key"
+  type = string
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type = string
+}
+
+variable "ec2_name" {
+  description = "EC2 Name"
+  type = string
+}
+
+variable "key_name" {
+  description = "AWS Key Name"
+  type = string
+}
